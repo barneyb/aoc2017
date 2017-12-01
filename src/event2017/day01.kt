771c30aba21fc799ccfe2dfa27262c117c8fd450
Barney Boisvert <bboisvert@barneyb.com>
2017-12-01 13:07:36 -0800

    refactored the captcha algorithm out into a factory function which is parameterized with how to wrap the string around for pairing characters

--- src/event2017/day01.kt
+++ src/event2017/day01.kt
@@ -8,13 +8,11 @@ import java.io.File
  * @author barneyb
  */
 
-val CHAR_ZERO = 48
-
 fun main(args: Array<String>) {
     val input = File("input/2017/day01.txt").readText().trim()
 
     banner("part 1")
-    val partOne = check(::captcha)
+    val partOne = check(captcha)
     partOne("1122", 3)
     partOne("1111", 4)
     partOne("1234", 0)
@@ -23,7 +21,7 @@ fun main(args: Array<String>) {
     println("answer: " + captcha(input))
 
     banner("part 2")
-    val partTwo = check(::captcha2)
+    val partTwo = check(captcha2)
     partTwo("1212", 6)
     partTwo("1221", 0)
     partTwo("123425", 4)
@@ -37,21 +35,20 @@ fun banner(label:String) {
     println("= ${label} ".padEnd(80, '='))
 }
 
-fun <T> check(functionUnderTest:(String) -> T)  = { input:String, expected:T ->
+fun <T> check(functionUnderTest:(String) -> T) = { input:String, expected:T ->
         val actual = functionUnderTest(input)
         if (actual != expected)
             println("Expected '$expected' but got '$actual' (from '$input')");
     }
 
-fun captcha(str:String):Int {
-    return str.fold(Pair(str.last(), 0), { a, c ->
-        Pair(c, a.second + if (c == a.first) (c.toInt() - CHAR_ZERO) else 0)
-    }).second
-}
+val captcha = captchaFactory { it.last().toString().plus(it) }
 
-fun captcha2(str:String):Int {
-    val pivot = str.length / 2
-    return str.foldIndexed(0, { i, a, c ->
-        a + if (c == str.get((i + pivot) % str.length)) (c.toInt() - CHAR_ZERO) else 0
-    })
+val captcha2 = captchaFactory { it.substring(it.length / 2).plus(it) }
+
+fun captchaFactory(pairer:(String) -> String) = { input:String ->
+    input.zip(pairer(input))
+            .filter { it.first == it.second }
+            .map { it.first }
+            .map { it.toInt() - 48 } // codepoint of '0'
+            .sum()
 }
