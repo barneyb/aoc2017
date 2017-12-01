820c83e7b1c9631b0cc74935254a31a5510a0cd9
Barney Boisvert <bboisvert@barneyb.com>
2017-12-01 11:40:36 -0800

    semi-curry `check` and partially apply it for the two parts

--- src/event2017/day01.kt
+++ src/event2017/day01.kt
@@ -14,18 +14,20 @@ fun main(args: Array<String>) {
     val input = File("input/2017/day01.txt").readText().trim()
 
     banner("part 1")
-    check(::captcha, "1122", 3)
-    check(::captcha, "1111", 4)
-    check(::captcha, "1234", 0)
-    check(::captcha, "91212129", 9)
+    val partOne = check(::captcha)
+    partOne("1122", 3)
+    partOne("1111", 4)
+    partOne("1234", 0)
+    partOne("91212129", 9)
     println("answer: " + captcha(input))
 
     banner("part 2")
-    check(::captcha2, "1212", 6)
-    check(::captcha2, "1221", 0)
-    check(::captcha2, "123425", 4)
-    check(::captcha2, "123123", 12)
-    check(::captcha2, "12131415", 4)
+    val partTwo = check(::captcha2)
+    partTwo("1212", 6)
+    partTwo("1221", 0)
+    partTwo("123425", 4)
+    partTwo("123123", 12)
+    partTwo("12131415", 4)
     println("answer: " + captcha2(input))
 }
 
@@ -33,11 +35,11 @@ fun banner(label:String) {
     println("= ${label} ".padEnd(80, '='))
 }
 
-fun <T> check(functionUnderTest:(String) -> T, input:String, expected:T) {
-    val actual = functionUnderTest(input)
-    if (actual != expected)
-        println("Expected '$expected' but got '$actual' (from '$input')");
-}
+fun <T> check(functionUnderTest:(String) -> T)  = { input:String, expected:T ->
+        val actual = functionUnderTest(input)
+        if (actual != expected)
+            println("Expected '$expected' but got '$actual' (from '$input')");
+    }
 
 fun captcha(str:String):Int {
     return str.fold(Pair(str.last(), 0), { a, c ->
