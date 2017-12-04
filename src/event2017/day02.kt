d11d18fa4061bc7e467b499890261685faecf486
Barney Boisvert <bboisvert@barneyb.com>
2017-12-03 16:02:56 -0800

    refactored to build part two

--- src/event2017/day02.kt
+++ src/event2017/day02.kt
@@ -13,15 +13,23 @@ fun main(args: Array<String>) {
     val input = File("input/2017/day02.txt").readText().trim()
 
     banner("part 1")
-    val partOne = check(::checksum)
+    val partOne = check(checksum1)
     partOne("5 1 9 5\n" +
             "7 5 3\n" +
             "2 4 6 8", 18)
     partOne(input, 53978)
-    println("answer: " + checksum(input))
+    println("answer: " + checksum1(input))
+
+    banner("part 2")
+    val partTwo = check(checksum2)
+    partTwo("5 9 2 8\n" +
+            "9 4 7 3\n" +
+            "3 8 6 5", 9)
+    println("answer: " + checksum2(input))
 }
 
-typealias Sheet = List<List<Int>>;
+typealias Row = List<Int>
+typealias Sheet = List<Row>;
 
 fun parse(sheet: String):Sheet {
     return sheet
@@ -36,10 +44,19 @@ fun parse(sheet: String):Sheet {
             }
 }
 
-fun checksum(sheet: String):Int {
-    return parse(sheet)
-            .map { row ->
-                row.max()!! - row.min()!!
-            }
-            .sum()
+val checksumFactory = { chksum: (Row) -> Int ->
+    { sheet: String ->
+        parse(sheet)
+                .map(chksum)
+                .sum()
+    }
 }
+
+val checksum1 = checksumFactory { row ->
+    row.max()!! - row.min()!!
+}
+
+val checksum2 = checksumFactory { row ->
+    row.max()!! - row.min()!!
+}
+
