fe43170d4a1c5017f587889d06a0e8cd7527f337
Barney Boisvert <bboisvert@barneyb.com>
2017-12-05 11:26:52 -0800

    marked day two's `parse` function private

--- src/event2017/day02.kt
+++ src/event2017/day02.kt
@@ -32,7 +32,7 @@ fun main(args: Array<String>) {
 typealias Row = List<Int>
 typealias Sheet = List<Row>;
 
-fun parse(sheet: String):Sheet {
+private fun parse(sheet: String):Sheet {
     return sheet
             .replace('\t', ' ')
             .trim()
