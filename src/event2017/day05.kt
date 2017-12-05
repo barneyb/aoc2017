db95bb8d53959bddf0494157bedcf9528666cb8f
Barney Boisvert <bboisvert@barneyb.com>
2017-12-05 11:58:12 -0800

    refactored to solve day five part two

--- src/event2017/day05.kt
+++ src/event2017/day05.kt
@@ -11,17 +11,34 @@ fun main(args: Array<String>) {
     val input = File("input/2017/day05.txt").readText().trim()
 
     banner("part one")
-    val partOne = check(::steps)
+    val partOne = check(::steps1)
     partOne("0\n" +
             "3\n" +
             "0\n" +
             "1\n" +
             "-3", 5)
     partOne(input, 375042)
-    println("answer: " + steps(input))
+    println("answer: " + steps1(input))
+
+    banner("part two")
+    val partTwo = check(::steps2)
+    partTwo("0\n" +
+            "3\n" +
+            "0\n" +
+            "1\n" +
+            "-3", 10)
+    partTwo(input, 28707598)
+    println("answer: " + steps2(input))
 }
 
-private fun steps(input:String):Int {
+private fun steps1(input:String):Int {
+    return stepsInternal(input) { it + 1 }
+}
+private fun steps2(input:String):Int {
+    return stepsInternal(input) { if (it >= 3) it - 1 else it + 1 }
+}
+
+private fun stepsInternal(input:String, munge:(Int) -> Int):Int {
     val instructions = input.split('\n')
             .map {
                 it.trim().toInt()
@@ -30,7 +47,7 @@ private fun steps(input:String):Int {
     var steps = 0
     while (pointer >= 0 && pointer < instructions.size) {
         val jump = instructions[pointer]
-        instructions[pointer] += 1
+        instructions[pointer] = munge(instructions[pointer])
         pointer += jump
         steps += 1
     }
