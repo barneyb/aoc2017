6c0324f866c9a38d905eed8d00316e8062bdabf1
Barney Boisvert <bboisvert@barneyb.com>
2017-12-05 11:36:05 -0800

    refactor and implement day four part two

--- src/event2017/day04.kt
+++ src/event2017/day04.kt
@@ -17,15 +17,38 @@ fun main(args: Array<String>) {
     partOne("aa bb cc dd aaa", 1)
     partOne(input, 455)
     println("answer: " + countValid(input))
+
+    banner("part 2")
+    val partTwo = check(::countValid2)
+    partTwo("abcde fghij", 1)
+    partTwo("abcde xyz ecdab", 0)
+    partTwo("a ab abc abd abf abj", 1)
+    partTwo("iiii oiii ooii oooi oooo", 1)
+    partTwo("oiii ioii iioi iiio", 0)
+    partTwo(input, 186)
+    println("answer: " + countValid2(input))
 }
 
 fun countValid(input:String):Int {
+    return process(input) { it }
+}
+
+private fun process(input: String, munge:(List<String>) -> List<String>):Int {
     return input.split('\n')
             .map {
                 it.split(' ')
             }
+            .map(munge)
             .filter {
                 it.size == it.distinct().size
             }
             .size
+}
+
+fun countValid2(input:String):Int {
+    return process(input) {
+                it.map {
+                    it.toCharArray().sorted().joinToString("")
+                }
+            }
 }
