bcb2b80206691fb9297161e46686bed6d0027774
Barney Boisvert <bboisvert@barneyb.com>
2017-12-06 21:37:27 -0800

    skeleton for day seven

---
+++ src/event2017/day07.kt
@@ -0,0 +1,34 @@
+package event2017
+
+import java.io.File
+
+/**
+ *
+ *
+ * @author barneyb
+ */
+fun main(args: Array<String>) {
+    val input = File("input/2017/day07.txt").readText().trim()
+
+    banner("part one")
+    val partOne = check(::findBase)
+    partOne("pbga (66)\n" +
+            "xhth (57)\n" +
+            "ebii (61)\n" +
+            "havc (66)\n" +
+            "ktlj (57)\n" +
+            "fwft (72) -> ktlj, cntj, xhth\n" +
+            "qoyq (66)\n" +
+            "padx (45) -> pbga, havc, qoyq\n" +
+            "tknk (41) -> ugml, padx, fwft\n" +
+            "jptl (61)\n" +
+            "ugml (68) -> gyxo, ebii, jptl\n" +
+            "gyxo (61)\n" +
+            "cntj (57)", "tknk")
+//    partOne(input, 6681)
+    println("answer: " + findBase(input))
+}
+
+fun findBase(input:String):String {
+    return input.split('\n').first()
+}
