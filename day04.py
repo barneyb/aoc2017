7592c0e3765c2ecf65311d091abc2c8dd851b3fc
Barney Boisvert <barneycode@barneyb.com>
2017-12-08 04:48:39 +0000

    day four part two

--- day04.py
+++ day04.py
@@ -18,3 +18,26 @@ for passphrase in passphrases:              # loop over each passphrase
         count += 1                          # ... record that this one is valid
 
 print "part one: " + str(count)
+
+count = 0
+for passphrase in passphrases:
+    words = []                              # where to put _sorted_ words
+    for word in passphrase.split():         # loop over the raw words
+        newWord = ""
+        for c in sorted(word):              # sort the letters and loop over
+            newWord += c                    # add it to 'newWord'
+        words.append(newWord)               # add to the list of words
+    # from here on down, it's _exactly_ the same as part one
+    wordCount = len(words)
+    foundDupe = False
+    for i in range(0, wordCount):
+        wordOne = words[i]
+        for j in range(i + 1, wordCount):
+            wordTwo = words[j]
+            if wordOne == wordTwo:
+                foundDupe = True
+
+    if not foundDupe:
+        count += 1
+
+print "part one: " + str(count)
