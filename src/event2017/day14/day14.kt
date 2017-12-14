7bd2b9fe3a4038ca2ea959d58685ec15fa5aeeca
Barney Boisvert <bboisvert@barneyb.com>
2017-12-14 08:03:53 -0800

    missing linebreak in chain

--- src/event2017/day14/day14.kt
+++ src/event2017/day14/day14.kt
@@ -29,4 +29,5 @@ fun blocksUsed(input: String) =
                                         }
                             }
                             .sum()
-                }.sum()
+                }
+                .sum()
