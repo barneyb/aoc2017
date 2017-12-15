bb7e311518d59c1e98478a58f3d0424576018f2b
Barney Boisvert <bboisvert@barneyb.com>
2017-12-15 08:27:36 -0800

    day fifteen part two

--- src/event2017/day15/day15.kt
+++ src/event2017/day15/day15.kt
@@ -20,27 +20,47 @@ fun main(args: Array<String>) {
     partOne(exampleInput, 588)
     partOne(input, 619)
 //    println("answer: " + matches(input))
+
+    banner("part 2")
+    val partTwo = check(::filteredMatches)
+    partTwo(exampleInput, 309)
+    partTwo(input, 290)
+//    println("answer: " + filteredMatches(input))
 }
 
-private fun parse(input: String) =
-        input.trim().split("\n").map { it.trim().split(" ").last().toLong() }
+typealias Generator = Sequence<Long>
+typealias GenPair = Pair<Generator, Generator>
+
+private fun parse(input: String): GenPair {
+    val seeds = input.trim().split("\n")
+            .map { it.trim().split(" ").last().toLong() }
+    return Pair(generator(seeds[0], 16807),
+            generator(seeds[1], 48271))
+}
 
 private fun generator(seed: Long, factor: Long) =
-    generateSequence(seed, { n ->
-        n * factor % 2147483647
-    })
-            .drop(1) // don't want the seed
-            .map {
-                // get the last 16 bits
-                it and 0xffff
-            }
+        generateSequence(seed, { n ->
+            n * factor % 2147483647
+        })
+                .drop(1) // don't want the seed
+                .map {
+                    // get the last 16 bits
+                    it and 0xffff
+                }
 
-fun matches(input: String): Int {
+private fun judge(p: GenPair, rounds: Int) =
+        p.first
+                .zip(p.second)
+                .take(rounds)
+                .count { (a, b) ->
+                    a == b
+                }
+
+fun matches(input: String) =
+        judge(parse(input), 40_000_000)
+
+fun filteredMatches(input: String): Int {
     val p = parse(input)
-    return generator(p.first(), 16807)
-            .zip(generator(p.last(), 48271))
-            .take(40_000_000)
-            .count {
-                (a, b) -> a == b
-            }
+    return judge(Pair(p.first.filter { it % 4 == 0L },
+            p.second.filter { it % 8 == 0L }), 5_000_000)
 }
