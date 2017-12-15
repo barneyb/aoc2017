6dbd0f76cf53104c9aacdb6b23f36d8912a920e3
Barney Boisvert <barneycode@barneyb.com>
2017-12-15 20:36:13 +0000

    removed timing output and set back to the demo seeds

--- day15.py
+++ day15.py
@@ -1,13 +1,12 @@
 from itertools import *
-import time
 
 # example input:
 seedA = 65
 seedB = 8921
 
 # real input:
-seedA = 591
-seedB = 393
+# seedA = 591
+# seedB = 393
 
 def gen(seed, factor):
     value = seed
@@ -20,16 +19,12 @@ def judge(a, b, i):
         lambda (x, y): x & 0xffff == y & 0xffff,
         islice(izip(a, b), i))))
 
-start = time.time()
 print judge(
         gen(seedA, 16807),
         gen(seedB, 48271),
         40000000)
-print str(time.time() - start) + " s"
 
-start = time.time()
 print judge(
         ifilter(lambda x: x % 4 == 0, gen(seedA, 16807)),
         ifilter(lambda x: x % 8 == 0, gen(seedB, 48271)),
         5000000)
-print str(time.time() - start) + " s"
