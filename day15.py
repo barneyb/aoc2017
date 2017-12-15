4074ee310f295e9a2dd6e3ee3638890537cc2b92
Barney Boisvert <barneycode@barneyb.com>
2017-12-15 20:35:35 +0000

    abstracted a judge function from the two parts

--- day15.py
+++ day15.py
@@ -6,8 +6,8 @@ seedA = 65
 seedB = 8921
 
 # real input:
-# seedA = 591
-# seedB = 393
+seedA = 591
+seedB = 393
 
 def gen(seed, factor):
     value = seed
@@ -15,20 +15,21 @@ def gen(seed, factor):
         value = (value * factor) % 2147483647
         yield value
 
+def judge(a, b, i):
+    return len(list(ifilter(
+        lambda (x, y): x & 0xffff == y & 0xffff,
+        islice(izip(a, b), i))))
+
 start = time.time()
-print len(list(ifilter(
-    lambda (x, y): x & 0xffff == y & 0xffff,
-    islice(izip(
+print judge(
         gen(seedA, 16807),
-        gen(seedB, 48271)
-        ), 40000000))))
+        gen(seedB, 48271),
+        40000000)
 print str(time.time() - start) + " s"
 
 start = time.time()
-print len(list(ifilter(
-    lambda (x, y): x & 0xffff == y & 0xffff,
-    islice(izip(
+print judge(
         ifilter(lambda x: x % 4 == 0, gen(seedA, 16807)),
         ifilter(lambda x: x % 8 == 0, gen(seedB, 48271)),
-        ), 5000000))))
+        5000000)
 print str(time.time() - start) + " s"
