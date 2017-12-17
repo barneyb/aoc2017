1da2da151f1dee84371b3cc09adebe25844c343a
Barney Boisvert <bboisvert@barneyb.com>
2017-12-17 08:01:33 -0800

    collapsed all the spins into offsetted exchanges and a final "cleanup" spin

--- src/event2017/day16.kt
+++ src/event2017/day16.kt
@@ -10,20 +10,23 @@ import java.io.File
 
 fun main(args: Array<String>) {
     val input = File("input/2017/day16.txt").readText()
-
-    banner("part 1")
-    val assertOne = check(::dance)
-    assertOne(input, "ebjpfdgmihonackl")
-    println("answer: " + dance(input))
-
     val dancers = "abcdefghijklmnop".toCharArray()
-    val targetRounds = 1000000000
-    val rounds = 100000
-    val start = System.currentTimeMillis()
-    println("$rounds rounds: " + aOneTwoThree(dancers, input, rounds))
-    val elapsed = System.currentTimeMillis() - start
-    println((1.0 * elapsed / 1000).toString() + " sec for " + rounds + " rounds; expecting " + 1.0 * elapsed / rounds * targetRounds / 1000.0 / 60.0 / 60.0 + " hrs for all " + targetRounds)
-    // estimate is 134 hours
+
+//    banner("part 1")
+//    val assertOne = check(::dance)
+//    assertOne(input, "ebjpfdgmihonackl")
+//    println("answer: " + dance(input))
+
+    aOneTwoThree(dancers, input, 20)
+
+//    val targetRounds = 1000000000
+//    val rounds = 100000
+//    val start = System.currentTimeMillis()
+//    println("$rounds rounds: " + aOneTwoThree(dancers, input, rounds))
+//    val elapsed = System.currentTimeMillis() - start
+//    println((1.0 * elapsed / 1000).toString() + " sec for " + rounds + " rounds; expecting " + 1.0 * elapsed / rounds * targetRounds / 1000.0 / 60.0 / 60.0 + " hrs for all " + targetRounds)
+    // 134 hours initially
+    // 82 hours with all the spins collapsed
 
 //    banner("part 2")
 //    val assertTwo = check(::partTwo)
@@ -34,10 +37,10 @@ fun main(args: Array<String>) {
 
 typealias Move = (CharArray) -> CharArray
 
-private fun parseMove(move: String): Move {
-    when (move[0]) {
+private fun parseMove(cmd: String, offset: Int): Move {
+    when (cmd[0]) {
         's' -> {
-            val n = move.drop(1).toInt()
+            val n = cmd.drop(1).toInt()
             return { dancers ->
                 val next = CharArray(16)
                 // copy tail -> head
@@ -48,9 +51,12 @@ private fun parseMove(move: String): Move {
             }
         }
         'x' -> {
-            val (i, j) = move.drop(1)
+            val (i, j) = cmd.drop(1)
                     .split('/')
-                    .map { it.toInt() }
+                    .map {
+                        val n = it.toInt() - offset
+                        if (n < 0) n + 16 else n
+                    }
             return { dancers ->
                 val c = dancers[i]
                 dancers[i] = dancers[j]
@@ -59,7 +65,7 @@ private fun parseMove(move: String): Move {
             }
         }
         'p' -> {
-            val (a, b) = move.drop(1)
+            val (a, b) = cmd.drop(1)
                     .split('/')
                     .map { it[0] }
             return { dancers ->
@@ -72,26 +78,69 @@ private fun parseMove(move: String): Move {
             }
         }
         else -> {
-            println("unknown move: '$move'")
+            println("unknown move: '$cmd'")
             return { ds -> ds }
         }
     }
 }
 
-private fun parse(input: String): List<Move> =
-        input.trim()
-                .split(',')
-                .map(::parseMove)
+private fun parse(input: String): List<Move> {
+    val (moves, offset) = input.trim()
+            .split(',')
+            .fold(Pair(mutableListOf<Move>(), 0), { (ms, o), cmd ->
+                if (cmd[0] == 's') {
+                    val n = cmd.drop(1).toInt()
+                    Pair(ms, (o + n) % 16)
+                } else {
+                    ms.add(parseMove(cmd, o))
+                    Pair(ms, o)
+                }
+            })
+    if (offset != 0) {
+        moves.add(parseMove("s" + offset, offset))
+    }
+    return moves
+}
 
 private fun dance(input: String) =
         aOneTwoThree("abcdefghijklmnop".toCharArray(), input)
 
+private val expecteds = listOf(
+        "ebjpfdgmihonackl",
+        "dblfiegmjknpohac",
+        "anbfdigmcokeplhj",
+        "eocfhpgainmdkljb",
+        "fbpiengdjmoaklch",
+        "dnjceogmlphfkbia",
+        "pkjfebgacinhmold",
+        "aojifkgehcnbdlpm",
+        "apljchgdifnokbme",
+        "abocefghijklmndp",
+        "ebjpfcgmihdnaokl",
+        "cblfiegmjknpdhao",
+        "anbfcigmodkeplhj",
+        "edofhpgainmckljb",
+        "fbpiengcjmdakloh",
+        "cnjoedgmlphfkbia",
+        "pkjfebgaoinhmdlc",
+        "adjifkgehonbclpm",
+        "apljohgcifndkbme",
+        "abdoefghijklmncp"
+).map { it.toCharArray() }
+
 private fun aOneTwoThree(initialDancers: CharArray, input: String, rounds: Int = 1): String {
     val moves = parse(input)
-    return IntRange(1, rounds).fold(initialDancers, { roundDancers, _ ->
-        moves.fold(roundDancers, { moveDancers, m ->
-            m(moveDancers)
-        })
+    return IntRange(1, rounds).fold(initialDancers, { roundDancers, i ->
+        val r =
+                moves.fold(roundDancers, { moveDancers, m ->
+                    m(moveDancers)
+                })
+        if (r.contentEquals(expecteds[i - 1])) {
+            println("round ${i.toString().padStart(2, ' ')} passed: ${String(r)}")
+        } else {
+            println("round ${i.toString().padStart(2, ' ')} failed: ${String(r)} (expecting ${String(expecteds[i - 1])})")
+        }
+        r
     }).joinToString("")
 }
 
