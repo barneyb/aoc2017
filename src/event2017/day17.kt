f5a87dd462b04b25be82071a78ca99057ed4e0d1
Barney Boisvert <bboisvert@barneyb.com>
2017-12-17 12:18:59 -0800

    day seventeen part two

--- src/event2017/day17.kt
+++ src/event2017/day17.kt
@@ -14,18 +14,34 @@ fun main(args: Array<String>) {
     val assertOne = check(::partOne)
     assertOne(exampleInput, 638)
     assertOne(input, 1282)
+    println("answer: " + partOne(input))
 
     banner("part 1 (sequence)")
     val assertOneSeq = check(::partOneSeq)
     assertOneSeq(exampleInput, 638)
     assertOneSeq(input, 1282)
-//    println("answer: " + partOne(input))
+    println("answer: " + partOneSeq(input))
 
-//    banner("part 2")
-//    val assertTwo = check(::partTwo)
-//    assertTwo(exampleInput, 309)
-//    println("example: " + partTwo(exampleInput))
-////    assertTwo(input, 290)
+    banner("math sequence!")
+    val bad = seq2a(exampleInput)
+            .zip(seq2b(exampleInput))
+            .take(2017)
+            .find { (a, b) ->
+                a.second != b.second
+            }
+    if (bad != null) {
+        val (a, b) = bad
+        if (a.first != b.first) {
+            println("indexes don't match! expected $a got $b")
+        }
+        println("FAILURE at n=${a.first} expected '${a.second}', but got '${b.second}")
+    } else {
+        println("PASS!")
+    }
+
+    banner("part 2")
+    val assertTwo = check(::partTwo)
+    assertTwo(input, 27650600)
 //    println("answer: " + partTwo(input))
 }
 
@@ -61,5 +77,25 @@ private fun seq1(input: Int) =
             Triple(nb, np, i + 1)
         })
 
-//private fun partTwo(input: Int) =
-//        input
+private fun seq2a(input: Int) =
+        seq1(input)
+                .drop(1)
+                .map { (b, _, i) ->
+                    Pair(i, b[1])
+                }
+
+private fun seq2b(input: Int) =
+        generateSequence(Triple(1, 1, 1), { (i, p, value) ->
+            val np = (p + input) % i + 1
+            Triple(i + 1, np, if (np == 1) i else value)
+        })
+                .drop(1)
+                .map {
+                    Pair(it.first, it.third)
+                }
+
+private fun partTwo(input: Int) =
+        seq2b(input)
+                .dropWhile { it.first < 50_000_000 }
+                .first()
+                .second
