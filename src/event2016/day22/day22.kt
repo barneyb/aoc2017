f028f97dbcde491188d3af4b77b7aa120054efdc
Barney Boisvert <bboisvert@barneyb.com>
2017-12-19 06:15:49 -0800

    set up for 2016's day twenty-two part two

--- src/event2016/day22/day22.kt
+++ src/event2016/day22/day22.kt
@@ -19,11 +19,23 @@ fun main(args: Array<String>) {
     assertOne(input, 864)
     println("answer: " + partOne(input))
 
-//    banner("part 2")
-//    val assertTwo = check(::partTwo)
-//    assertTwo(exampleInput, 7)
-////    assertTwo(input, 290)
-//    println("answer: " + partTwo(input))
+    banner("part 2")
+    val assertTwo = check(::partTwo)
+    assertTwo("root@ebhq-gridcenter# df -h\n" +
+            "Filesystem            Size  Used  Avail  Use%\n" +
+            "/dev/grid/node-x0-y0   10T    8T     2T   80%\n" +
+            "/dev/grid/node-x0-y1   11T    6T     5T   54%\n" +
+            "/dev/grid/node-x0-y2   32T   28T     4T   87%\n" +
+            "/dev/grid/node-x1-y0    9T    7T     2T   77%\n" +
+            "/dev/grid/node-x1-y1    8T    0T     8T    0%\n" +
+            "/dev/grid/node-x1-y2   11T    7T     4T   63%\n" +
+            "/dev/grid/node-x2-y0   10T    6T     4T   60%\n" +
+            "/dev/grid/node-x2-y1    9T    8T     1T   88%\n" +
+            "/dev/grid/node-x2-y2    9T    6T     3T   66%",
+            7
+    )
+//    assertTwo(input, 290)
+    println("answer: " + partTwo(input))
 }
 
 private data class Node(
@@ -60,5 +72,5 @@ private fun partOne(input: String): Int {
             }
 }
 
-//private fun partTwo(input: String) =
-//        input.length
+private fun partTwo(input: String) =
+        input.length
