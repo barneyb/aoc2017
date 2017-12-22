bece1cf59ee8cd6742b9efef983c12945351a467
Barney Boisvert <bboisvert@barneyb.com>
2017-12-22 06:21:10 -0800

    uncomment part two's stuff in the template

--- src/event2017/template.kt
+++ src/event2017/template.kt
@@ -18,15 +18,15 @@ fun main(args: Array<String>) {
 //    assertOne(input, 619)
     println("answer: " + partOne(input))
 
-//    banner("part 2")
-//    val assertTwo = check(::partTwo)
-//    assertTwo(exampleInput, 309)
-////    assertTwo(input, 290)
-//    println("answer: " + partTwo(input))
+    banner("part 2")
+    val assertTwo = check(::partTwo)
+    assertTwo(exampleInput, 309)
+//    assertTwo(input, 290)
+    println("answer: " + partTwo(input))
 }
 
 private fun partOne(input: String) =
         input.length
 
-//private fun partTwo(input: String) =
-//        input.length
+private fun partTwo(input: String) =
+        input.length
