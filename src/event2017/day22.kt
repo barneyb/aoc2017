6374006b1176dc8c3ebd0821352b4bb5656e7784
Barney Boisvert <bboisvert@barneyb.com>
2017-12-22 09:11:14 -0800

    removed the remaining "pure" buts from Carrier, parse, and partAnyFac

--- src/event2017/day22.kt
+++ src/event2017/day22.kt
@@ -32,82 +32,68 @@ fun main(args: Array<String>) {
 }
 
 private open class Carrier(
-        val p: Point,
-        val d: Direction
+        var p: Point,
+        var d: Direction = Direction.UP,
+        var infectCount: Int = 0
 ) {
-    // psuedo-emulate a data class's copy method
-    protected open fun dupe(p: Point, d: Direction) =
-            Carrier(p, d)
+    fun turnRight() {
+        d = d.turnRight()
+    }
 
-    fun turnRight() = dupe(p, d.turnRight())
+    fun turnLeft() {
+        d = d.turnLeft()
+    }
 
-    fun turnLeft() = dupe(p, d.turnLeft())
+    fun reverse() {
+        d = d.reverse()
+    }
 
-    fun reverse() = dupe(p, d.reverse())
+    fun step() {
+        p = p.step(d)
+    }
 
-    fun step() = dupe(p.step(d), d)
+    fun infect() {
+        infectCount += 1
+    }
 
-    open fun burst(s: GameState) =
-            if (p in s.cluster) {
-                s.cluster.remove(p)
-                GameState(
-                        s.cluster,
-                        turnRight().step(),
-                        s.infectCount
-                )
-            } else {
-                s.cluster.put(p, INFECTED)
-                GameState(
-                        s.cluster,
-                        turnLeft().step(),
-                        s.infectCount + 1
-                )
-            }
+    open fun burst(cluster: Cluster) {
+        if (p in cluster) {
+            cluster.remove(p)
+            turnRight()
+        } else {
+            cluster.put(p, INFECTED)
+            infect()
+            turnLeft()
+        }
+        step()
+    }
 }
 
 private class EvolvedCarrier(
-        p: Point,
-        d: Direction
-) : Carrier(p, d) {
-    // psuedo-emulate a data class's copy method
-    override fun dupe(p: Point, d: Direction) =
-            EvolvedCarrier(p, d)
+        p: Point
+) : Carrier(p) {
 
-    override fun burst(s: GameState) =
-            when (s.cluster.getOrDefault(p, CLEAN)) {
-                CLEAN -> {
-                    s.cluster.put(p, WEAK)
-                    GameState(
-                            s.cluster,
-                            turnLeft().step(),
-                            s.infectCount
-                    )
-                }
-                WEAK -> {
-                    s.cluster.put(p, INFECTED)
-                    GameState(
-                            s.cluster,
-                            step(),
-                            s.infectCount + 1
-                    )
-                }
-                INFECTED -> {
-                    s.cluster.put(p, FLAGGED)
-                    GameState(
-                            s.cluster,
-                            turnRight().step(),
-                            s.infectCount
-                    )
-                }
-                FLAGGED -> {
-                    s.cluster.remove(p)
-                    GameState(
-                            s.cluster,
-                            reverse().step(),
-                            s.infectCount
-                    )
-                }
+    override fun burst(cluster: Cluster) {
+        when (cluster.getOrDefault(p, CLEAN)) {
+            CLEAN -> {
+                cluster.put(p, WEAK)
+                turnLeft()
             }
+            WEAK -> {
+                cluster.put(p, INFECTED)
+                infect()
+            }
+            INFECTED -> {
+                cluster.put(p, FLAGGED)
+                turnRight()
+            }
+            FLAGGED -> {
+                cluster.remove(p)
+                reverse()
+            }
+        }
+        step()
+    }
 
 }
 
@@ -152,20 +138,15 @@ private data class GameState(
 }
 
 private fun partOneFac(iterations: Int) =
-        partAnyFac(iterations, { o -> Carrier(o, Direction.UP) })
+        partAnyFac(iterations, { o -> Carrier(o) })
 
 private fun partAnyFac(iterations: Int, carrierFactory: (Point) -> Carrier) = { input: String ->
     val (cluster, origin) = parse(input)
-//    val counter = generateSequence(1, { it + 1 }).iterator()
-    generateSequence(GameState(cluster, carrierFactory(origin)), { s ->
-//        val c = counter.next()
-//        if (c % 15_000 == 0)
-//            println("${c} bursts (${Math.round(100.0 * c / iterations)}%) w/ ${s.infectCount} infections")
-        s.carrier.burst(s)
-    })
-            .drop(iterations)
-            .first()
-            .infectCount
+    val carrier = carrierFactory(origin)
+    for (n in 1..iterations) {
+        carrier.burst(cluster)
+    }
+    carrier.infectCount
 }
 
 private typealias Cluster = MutableMap<Point, NodeState>
@@ -173,25 +154,23 @@ private typealias Cluster = MutableMap<Point, NodeState>
 private fun parse(input: String): Pair<Cluster, Point> {
     val lines = input.trim().split("\n")
     val o = (lines.size - 1) / 2
-    @Suppress("NAME_SHADOWING")
-    val infected = lines.foldIndexed(mapOf<Point, NodeState>(), { row, ps, line ->
+    val infected = mutableMapOf<Point, NodeState>()
+    lines.forEachIndexed { row, line ->
         // since "up" means "decrease y" in drawn-on-paper-land, but
-        // point uses cartesian ruls (up means increase y), just
+        // Point uses cartesian rules (up means increase y), just
         // flip the drawing and run with it.
         val flippedRow = lines.size - row - 1
-        line.foldIndexed(ps, { col, ps, c ->
+        line.forEachIndexed { col, c ->
             if (c == '#')
-                ps + (Point(col, flippedRow) to INFECTED)
-            else
-                ps
-        })
-    })
-    return Pair(infected.toMutableMap(), Point(o, o))
+                infected.put(Point(col, flippedRow), INFECTED)
+        }
+    }
+    return Pair(infected, Point(o, o))
 }
 
 private val partOne = partOneFac(10000)
 
 private fun partTwoFac(iterations: Int) =
-        partAnyFac(iterations, { o -> EvolvedCarrier(o, Direction.UP) })
+        partAnyFac(iterations, { o -> EvolvedCarrier(o) })
 
 private val partTwo = partTwoFac(10_000_000)
