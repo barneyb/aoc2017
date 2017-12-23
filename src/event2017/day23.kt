21586f43e88a56a86d5cf2d2e1e8928144bc10e3
Barney Boisvert <bboisvert@barneyb.com>
2017-12-23 09:46:24 -0800

    manually implemented the assembly instructions. still too slow

--- src/event2017/day23.kt
+++ src/event2017/day23.kt
@@ -20,9 +20,10 @@ fun main(args: Array<String>) {
     println("answer: " + partOne(input))
 
     banner("part 2")
-    val assertTwo = check(::partTwo)
-    assertTwo(input, 290)
-    println("answer: " + partTwo(input))
+//    val assertTwo = check(::partTwo)
+//    assertTwo(input, 290)
+    println("answer (debug): " + runOnASIC(true))
+    println("answer: " + runOnASIC())
 }
 
 private fun partOne(input: String) =
@@ -35,6 +36,67 @@ private fun partOne(input: String) =
                 .last()
                 .first
 
+private fun runOnASIC(debug: Boolean = false): Int {
+    var b = 57
+    var c = b
+    var d: Int
+    var e: Int
+    var f: Int
+    var g: Int
+    var h = 0
+
+    if (!debug) {
+        b *= 100
+        b -= -100000
+        c = b
+        c -= -17000
+    }
+    do {
+        f = 1
+        d = 2
+        do {
+            e = 2
+            do {
+                g = d
+                g *= e
+                g -= b
+                if (g == 0) {
+                    f = 0
+                }
+                e -= -1
+                g = e
+                g -= b
+            } while (g != 0)
+            d -= -1
+            g = d
+            g -= b
+        } while (g != 0)
+        if (f == 0) {
+            h -= -1
+        }
+        g = b
+        g -= c
+        println("b=$b, c=$c, d=$d, e=$e, f=$f, g=$g, h=$h")
+        if (g == 0) {
+            return h
+        }
+        b -= -17
+    } while (true)
+}
+
 private fun partTwo(input: String) =
-        input.length
+        generateSequence(Pair(1L,
+                Computer(instructions = loadDuet(input))
+                        .set('a', 1)), { (tc, comp) ->
+            if (tc % 50_000_000 == 0L) {
+                println("${tc / 1_000_000} M ) $comp")
+            }
+            if (comp.terminated)
+                null
+            else
+                Pair(tc + 1, comp.tick())
+        })
+                .last()
+                .second
+                .get('h')
 
