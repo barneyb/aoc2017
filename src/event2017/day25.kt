fe8880345c3fd01cc07e4485d9b01684dfcee50a
Barney Boisvert <bboisvert@barneyb.com>
2017-12-25 07:54:10 -0800

    day twenty-five parsing complete

---
+++ src/event2017/day25.kt
@@ -0,0 +1,115 @@
+package event2017
+
+import java.io.File
+
+/**
+ *
+ *
+ * @author barneyb
+ */
+
+fun main(args: Array<String>) {
+    val input = File("input/2017/day25.txt").readText()
+    val exampleInput = "Begin in state A.\n" +
+            "Perform a diagnostic checksum after 6 steps.\n" +
+            "\n" +
+            "In state A:\n" +
+            "  If the current value is 0:\n" +
+            "    - Write the value 1.\n" +
+            "    - Move one slot to the right.\n" +
+            "    - Continue with state B.\n" +
+            "  If the current value is 1:\n" +
+            "    - Write the value 0.\n" +
+            "    - Move one slot to the left.\n" +
+            "    - Continue with state B.\n" +
+            "\n" +
+            "In state B:\n" +
+            "  If the current value is 0:\n" +
+            "    - Write the value 1.\n" +
+            "    - Move one slot to the left.\n" +
+            "    - Continue with state A.\n" +
+            "  If the current value is 1:\n" +
+            "    - Write the value 1.\n" +
+            "    - Move one slot to the right.\n" +
+            "    - Continue with state A.\n"
+
+    banner("part 1")
+    val assertOne = check(::partOne)
+    assertOne(exampleInput, 3)
+//    assertOne(input, 619)
+//    println("answer: " + partOne(input))
+
+//    banner("part 2")
+//    val assertTwo = check(::partTwo)
+//    assertTwo(exampleInput, 309)
+////    assertTwo(input, 290)
+//    println("answer: " + partTwo(input))
+}
+
+private data class Blueprint(
+        val initialState: State,
+        val checksumCount: Int
+)
+
+private data class State(
+        var ifFalse: Action?,
+        var ifTrue: Action?
+)
+
+private data class Action(
+        val write: Boolean,
+        val left: Boolean,
+        var next: State?
+)
+
+private fun parse(input: String): Blueprint {
+    val lines = input.trim()
+            .split("\n")
+            .map { it.trim() }
+            .iterator()
+    val initial = lines.next()
+            .split(' ')
+            .last()[0]
+    val chksum = lines.next()
+            .split(' ')
+            .drop(5)
+            .first()
+            .toInt()
+
+    val states = mutableMapOf<Char, State>()
+    while (lines.hasNext()) {
+        lines.next() // the blank one
+        val label = lines.next()
+                .split(' ')
+                .last()[0]
+        val (ifFalse, ifTrue) = (1..2).map {
+            lines.next() // the 'if' line
+            Action(
+                    lines.next()
+                            .split(' ')
+                            .last()[0] == '1',
+                    lines.next()
+                            .split(' ')
+                            .last()[0] == 'l',
+                    states.getOrDefault(
+                            lines.next()
+                                    .split(' ')
+                                    .last()[0],
+                            State(null, null)
+                    )
+            )
+        }
+        val s = states.getOrDefault(label, State(null, null))
+        s.ifFalse = ifFalse
+        s.ifTrue = ifTrue
+        states.put(label, s)
+    }
+
+    return Blueprint(states.get(initial)!!, chksum)
+}
+
+private fun partOne(input: String) =
+        parse(input).checksumCount
+
+private fun partTwo(input: String) =
+        input.length
